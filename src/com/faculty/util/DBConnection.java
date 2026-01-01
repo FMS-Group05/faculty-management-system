dcd8f1eecf691c8df08296897fa7e80eb30c0af7
Rama <ravindukalhara215@gmail.com>
2026-01-01 13:45:13 +0530

    update data base

--- src/com/faculty/util/DBConnection.java
+++ src/com/faculty/util/DBConnection.java
@@ -5,18 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // Database credentials...........
-    private static final String URL = "jdbc:mysql://localhost:3306/login_db";
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/login_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
     private static final String USER = "root";
-    private static final String PASS = "Ravindu22520";
+    private static final String PASSWORD = "Ravindu22520";
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASS);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL Driver not found!");
+            throw new SQLException("MySQL Driver not found!", e);
         }
     }
-
 }
