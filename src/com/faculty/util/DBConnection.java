8217448c0a2dd4b05020083d4db31453a96ebab7
Rama <ravindukalhara215@gmail.com>
2026-01-01 12:41:15 +0530

    set update

--- src/com/faculty/util/DBConnection.java
+++ src/com/faculty/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-// Database credentials.......
+// Database credentials...........
     private static final String URL = "jdbc:mysql://localhost:3306/login_db";
     private static final String USER = "root";
     private static final String PASS = "Ravindu22520";
