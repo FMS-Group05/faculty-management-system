2cc41a086d70d623e4fdae5e5df493a3e6f08bcb
Sandun Deshan <deshansandun961@gmail.com>
2026-01-01 10:24:26 +0530

    gui basic set

--- src/com/faculty/util/DBConnection.java
+++ src/com/faculty/util/DBConnection.java
@@ -2,19 +2,20 @@ package com.faculty.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/fms_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/faculty_db";
     private static final String USER = "root";
     private static final String PASS = "";
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found!");
         }
     }
 }
